2e06fd81f39468a3fc6b0cc2844f38186cc416f1
Yuliia Savchenkova <Yuliia.Savchenkova@externals.adidas.com>
2020-10-20 23:32:15 +0300

    add  'Angular Matirial'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { FirstComponentComponent } from './components/first-component/first-comp
     FirstComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
